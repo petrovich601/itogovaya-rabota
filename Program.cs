a725843622dd17e8871031fb046cf75101fc6b66
jury <kulibin.76@mail.ru>
2022-11-27 21:57:00 +0500

    Diagramm

--- Program.cs
+++ Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите количество элементов массива:");
+int m = Convert.ToInt32(Console.ReadLine());
+if (m > 0)
+{
+Console.WriteLine("Введите элементы массива:");
+string[] array = new string[m];
+    for (int i = 0; i < m; i++)
+    {
+        array[i] = Console.ReadLine();
+    }
+    Console.WriteLine($"Элементы 3 и менее:");
+    for (int j = 0; j < m; j++)
+    {
+        if (array[j].Length <= 3)
+        {
+           
+            Console.WriteLine(array[j]); 
+        }   
+    }
+}
+else Console.WriteLine("Error");
